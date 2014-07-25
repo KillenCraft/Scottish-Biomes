eee7ef4233afbaf88a340e4ded46ecde4db1413e
Mooncoder <nashm@hotmail.com>
2014-07-24 22:33:19 -0500

    Forgot to remove the NetworkMod import becuase it's us

--- common/scottishbiomes/Loader.java
+++ common/scottishbiomes/Loader.java
@@ -21,7 +21,6 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = Loader.MODID, name = "Scottish Biomes",
